a7b49cc148e22c76dd0887f6f697ca7996eca1fc
Bradley Owens <bradowens17@hotmail.com>
2023-01-27 20:16:01 +1100

    add networks and second chances as the third and fourth project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,8 @@ import styles from "./Projects.module.css";
 
 import project1ImgSrc from "../../IMG/moka-bar.png";
 import project2ImgSrc from "../../IMG/gotya-plumbing.png";
+import project3ImgSrc from "../../IMG/networks.png";
+import project4ImgSrc from "../../IMG/second-chances.png";
 
 const Projects = () => {
   const hStyle = { color: "#ffd60a" };
@@ -17,7 +19,7 @@ const Projects = () => {
       imgSrc: project1ImgSrc,
       type: "Freelance Project",
       title: "Moka Bar",
-      technologies: ["HTML", "CSS", "JavaScript", "NextJS"],
+      technologies: ["HTML", "CSS", "JavaScript", "NextJS", "GoogleMaps API"],
       info: " Moka Bar is a website made for the moka bar cafe in Cairns QLD.This website was built with NextJS to optimize server side rendering and routes to the multiple pages it provides.",
       animationDirectionLeft: -80,
       animationDirectionRight: 80,
@@ -28,8 +30,30 @@ const Projects = () => {
       imgSrc: project2ImgSrc,
       type: "Freelance Project",
       title: "Gotya Plumbing Solutions",
-      technologies: ["HTML", "CSS", "JavaScript", "ReactJS"],
+      technologies: ["HTML", "CSS", "JavaScript", "ReactJS", "GoogleMaps API"],
       info: "Gotya Plumbing is a website made for the Gotya Plumbing business in QLD. This website was built with ReactJS as client only requested as a single page website. The website displays all services, reviews and contact information necessary for any client.",
+      animationDirectionLeft: 80,
+      animationDirectionRight: -80,
+      textAlign: projectAlignLeft,
+      flexDirection: projectFlexReverse,
+    },
+    {
+      imgSrc: project3ImgSrc,
+      type: "Personal Project",
+      title: "Networks",
+      technologies: ["HTML", "CSS", "JavaScript", "ReactJS", "Firebase"],
+      info: "Networks is an applications where users can create accounts/login and network with other developers by following and unfollowing to connect through other means of social media. This application implicates the use of authorisation, editing user data and storing with the use of redux and Firebase/Firebase Storage.",
+      animationDirectionLeft: -80,
+      animationDirectionRight: 80,
+      textAlign: projectAlignRight,
+      flexDirection: projectFlexNormal,
+    },
+    {
+      imgSrc: project4ImgSrc,
+      type: "Personal Project",
+      title: "Second Chances",
+      technologies: ["HTML", "CSS", "JavaScript", "ReactJS", "PetFinder API"],
+      info: "Second Chances is an application in which users can search and filter through adoptable pets such as cats and dogs to find the perfect friend. This application was built with react to cement the use of passing state through compoenents and the Petfinder API to use the JSON data to render throughout the application.",
       animationDirectionLeft: 80,
       animationDirectionRight: -80,
       textAlign: projectAlignLeft,
